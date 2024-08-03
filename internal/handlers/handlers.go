db671bd7f67fbf357e09f116aa61573a92d04888
Mikhail Kislov <kislov.mik@yandex.ru>
2024-08-03 03:19:47 +0300

    initial commit

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"LearnTest/internal/storage"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func MainHandle(w http.ResponseWriter, req *http.Request) {
+	countStr := req.URL.Query().Get("count")
+	if countStr == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("count missing"))
+		return
+	}
+
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("wrong count value"))
+		return
+	}
+
+	city := req.URL.Query().Get("city")
+
+	cafe, ok := storage.CafeList[city]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("wrong city value"))
+		return
+	}
+
+	if count > len(cafe) {
+		count = len(cafe)
+	}
+
+	answer := strings.Join(cafe[:count], ",")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(answer))
+}
